76bbedf5e047f4e9e3e767f98d66efe479b20b7c
Alyson Ryan Ullirsch <sdhuahdsaih@gmail.com>
2025-04-28 00:33:52 -0300

    terminar segundo calculo

---
+++ main.js
@@ -0,0 +1,34 @@
+class Cpf{
+    constructor(cpf){
+        this.cpf = cpf;
+    }
+
+    calcularPrimeiroDigito(cpf){
+        let contador = 10;
+        let soma = 0;
+        for(let i=0; i<9; i++){
+            let numero = parseInt(cpf[i]);
+            soma += numero * contador;
+            contador--;
+        }
+
+        let primeiroDigito = soma % 11;
+        if (primeiroDigito < 2){
+            primeiroDigito = 0;
+        }
+
+        var semiCpf = cpf + primeiroDigito.toString();
+
+        return semiCpf;
+    }
+
+    calcularCpf(){
+        let cpf = this.calcularPrimeiroDigito(this.cpf);
+        let contador = 11;
+
+        console.log(cpf);
+    }
+}
+
+cpf = new Cpf("102663849");
+cpf.calcularCpf();
